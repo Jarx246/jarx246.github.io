df4ee42eb696f8d51c4657649e97af2aaf277be7
Jarx246 <51779056+Jarx246@users.noreply.github.com>
2025-12-10 06:36:25 +0800

    style: add transparency to background

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
-import { ripple } from "@ripple-ts/vite-plugin";
+import { defineConfig } from 'vite';
+import { ripple } from '@ripple-ts/vite-plugin';
 
 export default defineConfig({
-  base: "/",
-  plugins: [ripple()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: "esnext",
-  },
+	base: '/',
+	plugins: [ripple()],
+	server: {
+		port: 3000,
+	},
+	build: {
+		target: 'esnext',
+	},
 });
